00048a5a2e2ecdc2b4eddd2d0518751874acef31
Xander Salathe <xsalathe@gmail.com>
2024-07-23 22:01:33 +1000

    feat: add bytetracker and linezone annotator instance for vehicle count

--- app.py
+++ app.py
@@ -13,26 +13,73 @@ import cv2
 
 # models will download on first use if not already installed locally
 
+# Load YOLO Model
 model = YOLO("yolov8m.pt")
-tracker = sv.ByteTrack()
+
+# Define vehicle class IDs (2: 'car', 3: 'motorcycle', 5: 'bus', 7: 'truck')
+selected_classes = [2, 3, 5, 7]
+
+# Define source and target video paths
+SOURCE_VIDEO_PATH = "./video/source_trimmed.mp4"
+TARGET_VIDEO_PATH = "./video/result.mp4"
+
+# Create VideoInfo instance
+video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
+
+# Create frame generator
+generator = sv.get_video_frames_generator(SOURCE_VIDEO_PATH)
+
+# Create ByteTracker Instance
+tracker = sv.ByteTrack(track_activation_threshold=0.25, lost_track_buffer=60, minimum_matching_threshold=0.8, frame_rate=30)
+
+# Create BoxAnnotator Instance
 box_annotator = sv.BoundingBoxAnnotator()
 
+# Define line zones for counting
+LINE_START_ONE = sv.Point(225, 325)
+LINE_END_ONE = sv.Point(0, 275)
+
+LINE_START_TWO = sv.Point(375, 200)
+LINE_END_TWO = sv.Point(270, 200)
+
+LINE_START_THREE = sv.Point(575, 280)
+LINE_END_THREE = sv.Point(425, 230)
+
+# Create LineZone Instances
+line_zones = [
+    sv.LineZone(start=LINE_START_ONE, end=LINE_END_ONE),
+    sv.LineZone(start=LINE_START_TWO, end=LINE_END_TWO),
+    sv.LineZone(start=LINE_START_THREE, end=LINE_END_THREE)
+]
+
+# Create LineZoneAnnotator instance
+line_zone_annotator = sv.LineZoneAnnotator(thickness=1, text_thickness=1)
+
+# Filter detections to only keep vehicles
 def filter_vehicle_detections(detections):
-    # Vehicle class IDs (2: 'car', 3: 'motorcycle', 5: 'bus', 7: 'truck')
-    selected_classes = [2, 3, 5, 7]
-    detections = detections[detections.confidence > 0.6]
     vehicle_detections = detections[np.isin(detections.class_id, selected_classes)]
     return vehicle_detections
 
 def callback(frame: np.ndarray, _: int) -> np.ndarray:
+    # Model prediction on single frame and conversion to supervision Detections
     results = model(frame)[0]
     detections = sv.Detections.from_ultralytics(results)
     # Filter only vehicle detections
     detections = filter_vehicle_detections(detections)
     # Update tracker with vehicle detections
     detections = tracker.update_with_detections(detections)
-    # Annotate frame with vehicle detections only
-    return box_annotator.annotate(frame.copy(), detections=detections)
+
+    annotated_frame = box_annotator.annotate(scene=frame.copy(), detections=detections)
+
+    # Update line counters
+    for line_zone in line_zones:
+        line_zone.trigger(detections)
+
+    # Annotate frame with line counters
+    for line_zone in line_zones:
+        annotated_frame = line_zone_annotator.annotate(annotated_frame, line_counter=line_zone)
+
+    return annotated_frame
 
 def process_video_with_progress(source_path: str, target_path: str, callback, progress_desc: str = "Processing"):
     cap = cv2.VideoCapture(source_path)
@@ -50,8 +97,9 @@ def process_video_with_progress(source_path: str, target_path: str, callback, pr
             callback=wrapped_callback
         )
 
+# Process the whole video
 process_video_with_progress(
-    source_path="./video/source_trimmed.mp4",
-    target_path="./video/result.mp4",
+    source_path=SOURCE_VIDEO_PATH,
+    target_path=TARGET_VIDEO_PATH,
     callback=callback
 )
